f01b8f0c76f1ceba55ce315ef7d43cdfa0a76102
tf153 <43797501+tf153@users.noreply.github.com>
2020-08-30 07:58:04 +0530

    local change

--- index.php
+++ index.php
@@ -8,21 +8,47 @@
     <button id="signup-btn" class="index-button">SignUp</button>
   </div>
 
-  <div id='login-box' class='pop-up'>
-    <div class='pop-up-block'>
-      <h3><center>Login</center></h3>
+  <div id='login-box'>
+    <div id='pop-up-login'>
+      <h2><center>Login</center></h2>
       <form action="#" method='post'>
-        <label for='username'>Username</label>
-        <input type='text' name='username' id='username' class='input' required placeholder="Username">
-        <label for='password'>Password</label>
-        <input type='password' name='password' id='password' class='input' required placeholder="Username">
+        <label for='username'>Username</label><br>
+        <input class='input-box' type='text' name='username' id='username' required placeholder="Username"><br>
+        <label for='password'>Password</label><br>
+        <input class='input-box' type='password' name='password' id='password' required placeholder="Password"><br>
+        <center><button id="submit-login" class="submit-button" type='submit'>Login</button></center>
       </form>
     </div>
   </div>
 
-  <div id='login-box' class='pop--up'>
+  <div id='signup-box'>
+    <div id='pop-up-signup'>
+      <h2><center>Signup</center></h2>
+      <form action="#" method='post'>
+        <label for='name'>Name</label><br>
+        <input class='input-box' type='text'name='name'id='name' required placeholder="Full Name"><br>
+        <label for='clg_id'>College ID</label><br>
+        <input class='input-box' type='text' name='clg_id' id='clg_id' required placeholder="Enter The College Referral Id"><br>
+        <label for='username'>Username</label><br>
+        <input class='input-box' type='text' name='username' id='username' required placeholder="Username"><br>
+        <label for='password'>Password</label><br>
+        <input class='input-box' type='password' name='password' id='password' required placeholder="Password"><br>
+        <center><button id="submit-signup" class="submit-button" type='submit'>Signup</button></center>
+      </form>
+    </div>
   </div>
 
 </div>
 <?php require_once 'footer.php';?>
-<script src="script_index.js"></script>
+<script >
+var login=document.getElemetById('login-btn');
+var signup=document.getElementByID('signup-btn');
+var login_popup=document.getElementById('login-box');
+var signup_popup=document .getElementById('signup-box');
+login.onclick=function(){
+  login_popup.style.display="block";
+}
+signup.onclick=function(){
+  signup_popup.style.display="block";
+}
+</script>
--- scripts/script_index.js
+++ scripts/script_index.js
@@ -1,5 +1,10 @@
-{
-  let login=document.getElemetById('login-btn');
-  let signup=document.getElementByID('signup-btn');
-
+var login=document.getElementById('login-btn');
+var signup=document.getElementById('signup-btn');
+var login_popup=document.getElementById('login-box');
+var signup_popup=document .getElementById('signup-box');
+login.onclick=function(){
+  login_popup.style.display="block";
+}
+signup.onclick=function(){
+  signup_popup.style.display="block";
 }
